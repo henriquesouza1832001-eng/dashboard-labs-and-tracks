be2da082507bb7d11d79f21e93c4cfdb7d3985ce
Henrique Souza <henriquesouza1832001@gmail.com>
2026-06-24 11:20:27 -0300

    serve todos estaticos na raiz

--- app.py
+++ app.py
@@ -1,17 +1,8 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 
 app = FastAPI()
-
-app.mount('/hub', StaticFiles(directory='ERPFiat-Portatil/resources/hub'), name='hub')
-app.mount('/chamados', StaticFiles(directory='ERPFiat-Portatil/resources/chamados'), name='chamados')
-app.mount('/obras', StaticFiles(directory='ERPFiat-Portatil/resources/obras'), name='obras')
-app.mount('/kpi', StaticFiles(directory='ERPFiat-Portatil/resources/kpi'), name='kpi')
-app.mount('/codins', StaticFiles(directory='ERPFiat-Portatil/resources/codins'), name='codins')
-app.mount('/conforto', StaticFiles(directory='ERPFiat-Portatil/resources/conforto'), name='conforto')
-app.mount('/atividades', StaticFiles(directory='ERPFiat-Portatil/resources/atividades'), name='atividades')
-app.mount('/js', StaticFiles(directory='ERPFiat-Portatil/resources/js'), name='js')
 
 @app.get('/')
 async def root():
@@ -20,4 +11,6 @@ async def root():
 @app.get('/health')
 async def health():
     return {'status': 'ok'}
+
+app.mount('/', StaticFiles(directory='ERPFiat-Portatil/resources', html=True), name='static')
 
