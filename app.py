5c3ccf498fefa328813a7ab39de693d40e9738c6
Henrique Souza <henriquesouza1832001@gmail.com>
2026-06-24 11:19:38 -0300

    corrige montagem de arquivos estaticos por modulo

--- app.py
+++ app.py
@@ -4,7 +4,14 @@ from fastapi.responses import FileResponse
 
 app = FastAPI()
 
-app.mount('/static', StaticFiles(directory='ERPFiat-Portatil/resources'), name='static')
+app.mount('/hub', StaticFiles(directory='ERPFiat-Portatil/resources/hub'), name='hub')
+app.mount('/chamados', StaticFiles(directory='ERPFiat-Portatil/resources/chamados'), name='chamados')
+app.mount('/obras', StaticFiles(directory='ERPFiat-Portatil/resources/obras'), name='obras')
+app.mount('/kpi', StaticFiles(directory='ERPFiat-Portatil/resources/kpi'), name='kpi')
+app.mount('/codins', StaticFiles(directory='ERPFiat-Portatil/resources/codins'), name='codins')
+app.mount('/conforto', StaticFiles(directory='ERPFiat-Portatil/resources/conforto'), name='conforto')
+app.mount('/atividades', StaticFiles(directory='ERPFiat-Portatil/resources/atividades'), name='atividades')
+app.mount('/js', StaticFiles(directory='ERPFiat-Portatil/resources/js'), name='js')
 
 @app.get('/')
 async def root():
