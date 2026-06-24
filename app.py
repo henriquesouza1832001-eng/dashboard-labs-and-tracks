83ecbb4ef409979f3b56ae0c0b4bce52c5b89213
Henrique Souza <henriquesouza1832001@gmail.com>
2026-06-24 11:18:30 -0300

    corrige rotas de arquivos estaticos

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-import os
 
 app = FastAPI()
 
-# Serve os arquivos estaticos do frontend
 app.mount('/static', StaticFiles(directory='ERPFiat-Portatil/resources'), name='static')
 
 @app.get('/')
@@ -16,10 +14,3 @@ async def root():
 async def health():
     return {'status': 'ok'}
 
-# Captura o usuario logado via Azure AD
-@app.middleware('http')
-async def add_user(request: Request, call_next):
-    user = request.headers.get('X-Forwarded-User', 'local-dev')
-    request.state.user = user
-    response = await call_next(request)
-    return response
