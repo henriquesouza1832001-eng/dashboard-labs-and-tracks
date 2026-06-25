e8f23e69eb407b8baf94039571b40d2022466b01
Henrique Souza <henriquesouza1832001@gmail.com>
2026-06-25 15:15:06 -0300

    fix: manifest trocado para /app.webmanifest em todos os HTMLs

--- ERPFiat-Portatil/resources/sw.js
+++ ERPFiat-Portatil/resources/sw.js
@@ -38,13 +38,15 @@ self.addEventListener('fetch', e => {
   const url = new URL(e.request.url);
  if (url.pathname.startsWith('/api/')) {
   e.respondWith(
-    fetch(e.request.clone())
+    fetch(e.request)
       .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+        if (res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then(c => c.put(e.request.url, clone));
+        }
         return res;
       })
-      .catch(() => caches.match(e.request))
+      .catch(() => caches.match(e.request.url))
   );
   return;
 }
