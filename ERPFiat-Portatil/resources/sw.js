6dbe5cb2ec3515b4beae7974329a2f3d05315468
Henrique Souza <henriquesouza1832001@gmail.com>
2026-07-20 13:43:35 -0300

    teste novo deploy.yml

--- ERPFiat-Portatil/resources/sw.js
+++ ERPFiat-Portatil/resources/sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE_NAME = 'controler-v1.3.26';
+const CACHE_NAME = 'controler-v1.3.27';
 const SHELL = [
   '/hub/hub.html', '/hub/hub.css', '/hub/hub.js',
   '/hub/auth.js', '/hub/api.js', '/hub/cache.js', '/hub/pwa.js',
